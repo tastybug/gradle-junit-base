2be0ec79eeab57be3f00a89ee31a488a4c44d9aa
Philipp Bartsch <tastybug@tastybug.com>
2021-09-17 14:30:56 +0200

    update deps: assertj, junit, gradle

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,11 +3,14 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void name() {
         assertThat("1").isEqualTo("1");
+        assertEquals("1", "1");
     }
+
 }
